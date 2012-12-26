0daaadf6ff51d7cd9f07bf3a0c4966ef4532e428
Ellen Gummesson <ellengummesson@gmail.com>
2012-12-26 13:48:41 +0100

    Added profile and updated paths.

--- .bash/path.sh
+++ .bash/path.sh
@@ -2,5 +2,17 @@
 # PATH
 #######
 
-# Add local bins to path
-export PATH="$PATH:/usr/local/bin"
+# Include private bin if it exists
+if [ -d "${HOME}/bin" ] ; then
+  PATH="${HOME}/bin:${PATH}"
+fi
+
+# Include private man if it exists
+if [ -d "${HOME}/man" ]; then
+  MANPATH="${HOME}/man:${MANPATH}"
+fi
+
+# Include private info if it exists
+if [ -d "${HOME}/info" ]; then
+  INFOPATH="${HOME}/info:${INFOPATH}"
+fi
---
+++ .bash_profile
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###############
+# BASH PROFILE
+###############
+
+# Source .bashrc if it exists
+if [ -f "${HOME}/.bashrc" ] ; then
+  source "${HOME}/.bashrc"
+fi
