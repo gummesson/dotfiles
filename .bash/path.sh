6ee12db7de3e3f2c6232c3c6e381acb4c8acd2d3
Ellen Gummesson <ellengummesson@gmail.com>
2012-12-27 09:19:16 +0100

    Updated path.sh

--- .bash/path.sh
+++ .bash/path.sh
@@ -4,17 +4,17 @@
 # PATH
 #######
 
-# Include private bin if it exists
+# Include private bin
 if [ -d "${HOME}/bin" ] ; then
   PATH="${HOME}/bin:${PATH}"
 fi
 
-# Include private man if it exists
+# Include private man
 if [ -d "${HOME}/man" ]; then
   MANPATH="${HOME}/man:${MANPATH}"
 fi
 
-# Include private info if it exists
+# Include private info
 if [ -d "${HOME}/info" ]; then
   INFOPATH="${HOME}/info:${INFOPATH}"
 fi
