3fb3bbb3efaaddbbe1d991bef0bf558897d45846
Ellen Gummesson <ellengummesson@gmail.com>
2013-08-07 13:59:21 +0200

    Update scripts.

--- scripts/dropbox.sh
+++ scripts/dropbox.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# == Setup ==
+
+sudo mkdir -p $HOME/.tools/dropbox-cli
+sudo cd $HOME/.tools/dropbox-cli
+
+sudo curl -O https://www.dropbox.com/download?dl=packages/dropbox.py
+sudo chmod +x dropbox.py
+sudo ln -s $HOME/.tools/dropbox-cli/dropbox.py /usr/bin/dropbox
+
 # == Files ==
 
 sudo ln -s $HOME/Dropbox/Dokument/Dotfiles/hnb/.hnb ~/.hnb
