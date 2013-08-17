2c2c356cf1833cc67e4fabf13218b30524899606
Ellen Gummesson <ellengummesson@gmail.com>
2013-08-17 19:19:32 +0200

    Add 'builds' script.

---
+++ scripts/builds.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# == Colors ==
+
+bold="\033[1m"
+red="\033[0;31m"
+reset="\033[0m"
+
+# == Repos ==
+
+repos=(
+  "dwm"
+  "dmenu"
+  "slock"
+)
+
+echo -e "${bold}Cloning:${reset} ${red}${repos[@]}${reset}"
+
+cd $HOME/test
+
+for repo in "${repos[@]}"; do
+  git clone http://git.suckless.org/"${repo}"
+done
