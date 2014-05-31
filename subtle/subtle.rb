007b39ac122551b36d46eb4f225fc67013f578fc
Ellen Gummesson <ellengummesson@gmail.com>
2014-05-31 15:54:41 +0200

    Rename script.

--- subtle/subtle.rb
+++ subtle/subtle.rb
@@ -137,7 +137,7 @@ gravity :bottom_right33, [  50,  67,  50,  33 ]
 # -- Programs --
 
 grab "W-Return", "urxvtc"
-grab "W-m",      "~/.bin/launcher"
+grab "W-m",      "~/.bin/launch"
 
 # -- Views --
 
