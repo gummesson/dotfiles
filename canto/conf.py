f2bb12a681b35cfdcef58268d6c42235aac02808
Ellen Gummesson <ellengummesson@gmail.com>
2014-06-03 21:20:00 +0200

    Fix comments.

--- canto/conf.py
+++ canto/conf.py
@@ -1,34 +1,55 @@
 # -- conf.py ------------------------------------------------------- {{{
+#
+# -- Imports
+# -- Settings
+# -- Hooks
+# -- Keybindings
+# -- Colors
+# -- Feeds
+#
+# }}}
 
-# -- Imports --
+# -- Imports ------------------------------------------------------- {{{
 
 from canto.extra import *
 
-# -- Settings --
+# }}}
+
+# -- Settings ------------------------------------------------------ {{{
 
 default_rate(60)
 default_keep(10)
 never_discard("unread")
 link_handler("uzbl-browser \"%u\"")
 
-# -- Hooks --
+# }}}
+
+# -- Hooks --------------------------------------------------------- {{{
 
 start_hook = show_unread
 
-# -- Keybindings --
+# }}}
+
+# -- Keybindings --------------------------------------------------- {{{
 
 keys['-'] = set_filter(show_unread)
 keys['+'] = set_filter(None)
 
-# -- Colors --
+# }}}
 
-# Defaults
+# -- Colors -------------------------------------------------------- {{{
+
+# -- Defaults --
+
 colors[0] = ("default", "default")
 
-# Emphasis
+# -- Emphasis --
+
 colors[6] = ("default", "default")
 
-# -- Feeds --
+# }}}
+
+# -- Feeds --------------------------------------------------------- {{{
 
 source_urls("/home/ellen/.canto/urls")
 
