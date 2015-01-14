0f6dc05648133b39ecfb9ffca8272cf5811ff279
Ellen Gummesson <ellengummesson@gmail.com>
2015-01-14 19:09:00 +0100

    `build` -> `src`.

--- dwm/install.sh
+++ dwm/install.sh
@@ -4,12 +4,12 @@ set -e
 
 gitpath=$HOME/Git/dotfiles
 binpath=$HOME/.local/bin
-buildpath=$HOME/.local/builds/dwm
+srcpath=$HOME/.local/builds/dwm
 
-ln -vsf $gitpath/dwm/config.h $buildpath/config.h
+ln -vsf $gitpath/dwm/config.h $srcpath/config.h
 ln -vsf $gitpath/dwm/dwm-start $binpath/dwm-start
 
-cd $buildpath
+cd $srcpath
 git apply $gitpath/dwm/dwm-6.1-xft.diff
 git apply $gitpath/dwm/dwm-6.1-better-borders.diff
 
