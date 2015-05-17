89d5d7f1639a5039c76c90d464029ea9bd147a38
Ellen Gummesson <ellengummesson@gmail.com>
2015-05-17 11:06:44 +0200

    Remove `Chromium`.

--- frankenwm/config.h
+++ frankenwm/config.h
@@ -46,7 +46,6 @@
 static const AppRule rules[] = { \
     /* class              desktop  follow  float */
     { "Gvim",             0,       True,   True  },
-    { "Chromium",         1,       True,   False },
     { "Google Chrome",    1,       True,   False },
     { "zathura",          2,       True,   False },
     { "mpv",              2,       True,   False },
