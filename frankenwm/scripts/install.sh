a1a07523591c9d22bfe7623f3849a381ba2a96e7
Ellen Gummesson <ellengummesson@gmail.com>
2015-07-20 14:09:30 +0200

    Normalize

--- frankenwm/scripts/install.sh
+++ frankenwm/scripts/install.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-gitpath=$HOME/Git/dotfiles
-srcpath=$HOME/.local/src/frankenwm
+gitpath="${HOME}/Git/dotfiles"
+srcpath="${HOME}/.local/src/frankenwm"
 
 echo "Symlinking files..."
-ln -vsf $gitpath/frankenwm/config.h $srcpath/config.h
+ln -vsf "$gitpath/frankenwm/config.h" "$srcpath/config.h"
 
-cd $srcpath
+cd "$srcpath"
 sudo make clean install
--- irssi/scripts/install.sh
+++ irssi/scripts/install.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env sh
 
-scrpath=$HOME/.irssi/scripts/autorun
+scrpath="${HOME}/.irssi/scripts/autorun"
 
 curl --fail --location --output \
-  $scrpath/adv_windowlist.pl \
-  http://anti.teamidiot.de/static/nei/*/Code/Irssi/adv_windowlist.pl
+  "$scrpath/adv_windowlist.pl" \
+  "http://anti.teamidiot.de/static/nei/*/Code/Irssi/adv_windowlist.pl"
 
 curl --fail --location --output \
-  $scrpath/autoaway.pl \
-  http://scripts.irssi.org/scripts/autoaway.pl
+  "$scrpath/autoaway.pl" \
+  "http://scripts.irssi.org/scripts/autoaway.pl"
 
 curl --fail --location --output \
-  $scrpath/nickcolor.pl \
-  http://scripts.irssi.org/scripts/nickcolor.pl
+  "$scrpath/nickcolor.pl" \
+  "http://scripts.irssi.org/scripts/nickcolor.pl"
 
 curl --fail --location --output \
-  $scrpath/nm.pl \
-  http://scripts.irssi.org/scripts/nm.pl
+  "$scrpath/nm.pl" \
+  "http://scripts.irssi.org/scripts/nm.pl"
 
 curl --fail --location --output \
-  $scrpath/tmux_away.pl \
-  http://scripts.irssi.org/scripts/tmux_away.pl
+  "$scrpath/tmux_away.pl" \
+  "http://scripts.irssi.org/scripts/tmux_away.pl"
 
 curl --fail --location --output \
-  $scrpath/trackbar.pl \
-  http://scripts.irssi.org/scripts/trackbar.pl
+  "$scrpath/trackbar.pl" \
+  "http://scripts.irssi.org/scripts/trackbar.pl"
 
 curl --fail --location --output \
-  $scrpath/usercount.pl \
-  http://scripts.irssi.org/scripts/usercount.pl
+  "$scrpath/usercount.pl" \
+  "http://scripts.irssi.org/scripts/usercount.pl"
